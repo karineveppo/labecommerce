0792cd9986ae06f76131839b5b7f28381b5789fe
karineveppo <karineveppo83@gmail.com>
2023-01-27 14:31:51 -0300

    :sparkles: feat: criando arquivo database.ts. Criar um array para cada entidade e exportá-los.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("funcionando tudo?!?!")
+import { users, products, purchases } from "./database"
+
+console.log("Tabela de usuários")
+    console.table(users)
+
+console.log("Tabela de produtos")
+    console.table(products)
+
+console.log("Tabela de compras")
+    console.table(purchases)
